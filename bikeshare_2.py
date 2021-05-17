3210c8ccf7e3ceb514a81e023d5f1372f60a3548
Selaelo <slymudau@gmail.com>
2021-05-17 10:04:19 +0200

    Changed the description under get_fileter, from ask the user... to request the user...

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -9,7 +9,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Requst the user to specify a city, month, and day to analyze.
 
     Returns:
         (str) city - name of the city to analyze
